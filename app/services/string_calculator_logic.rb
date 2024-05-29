49b43dd79dc1edef9de0d12717fbaf78f1e1e45a
raksha <rubyrubs10@gmail.com>
2024-05-29 22:34:39 +0530

    delimeter string

--- app/services/string_calculator_logic.rb
+++ app/services/string_calculator_logic.rb
@@ -11,6 +11,10 @@ class StringCalculatorLogic
 
     numbers = numbers_string.gsub('\n', ",").split(',').map(&:to_i) if numbers_string.include?('\n')
 
+    numbers = numbers_string.split("\;").map {|x| x[/\d+/]}.map(&:to_i) if numbers_string.start_with?("//")
+
+    return "invalid" if numbers_string.split(',').include?('\n') 
+
     numbers.sum
   end
   
